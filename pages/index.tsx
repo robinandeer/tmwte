3994b9d3f2a5ad41d8e1df63ed72f96e500329e6
Robin Andeer <robin.andeer@gmail.com>
2022-04-19 09:07:20 +0200

    Fix iOS scroll issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen w-screen grid justify-center items-center bg-red-50">
+    <div className="grid justify-center items-center flex-grow">
       <form method="post" action="/api/restaurants/random">
         <button className="bg-red-600 text-white p-4 rounded-md text-lg active:scale-75 touch-manipulation shadow-xl shadow-red-300 transition-transform">
           Tell me where to eat!
